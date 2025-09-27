bb6b6944a53e10d7f0f25fb8accf5054003fe1d4
Jose Manuel <josemanuelsolisquesada@MacBook-Air-de-Jose-3.local>
2025-09-27 01:43:00 -0600

    Ejercicio corregido

---
+++ ejercicio1.c
@@ -0,0 +1,19 @@
+// Este script contiene el codigo del ejercicio 1
+
+#include <stdio.h>
+
+int factorial (int n){
+    int i = 1;
+    while (n > 1){
+        i = i * n;
+        n = n - 1;  // Se elimina el int que generaba el error en el codigo
+    }
+    return i;
+}
+
+int main (int argc, char *argv[]){
+    int fac4 = factorial(4);
+    int fac5 = factorial(5);
+    printf("4! = %d, 5! = %d\n", fac4, fac5);
+    return 0;
+}
