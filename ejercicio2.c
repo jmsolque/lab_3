26faee58f80d04766eddfb919a20984cfe134d1b
Jose Manuel <josemanuelsolisquesada@MacBook-Air-de-Jose-3.local>
2025-09-27 10:38:35 -0600

    Codigo dado en las instrucciones sin modificar

---
+++ ejercicio2.c
@@ -0,0 +1,24 @@
+// Este script contiene el codigo del ejercicio 2 del laboratorio 3
+
+#include <stdio.h>
+
+#define SIZE 5
+
+int findLargestLine(int matrix [][SIZE]){
+    // Si implementacion
+}
+
+int main (){
+    int matrix [SIZE][SIZE] = {
+        {2, 7, 6},
+        {9, 5, 1},
+        {4, 33, 8}
+    };
+
+    int largestLine = findLargestLine(matrix);
+    printf("El tamaño del a secuencia de ls mas grande es: %d\n", largestLine);
+    return 0;
+}
+
+
+
